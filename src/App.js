a7dbb7efdb02082035a6de3c565d9c2396836fac
Bruno Secchiari <bsecchia@ar.ibm.com>
2023-10-06 20:36:35 -0300

    Initializing Project

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+createBrowserRouter([{ path: '/' }]);
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
